fe5751f562ab136104f6672d358b07e3607bc167
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-14 21:07:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,10 +50,6 @@ end
 def find_a(array)
   new_array = []
   array.collect do |element|
-    if element[0] = "a"
-      new_array << element
-    elsif element[0] != "a"
-      new_array.drop
-  end
+    array.keep_if element[0] == "a"
 end
 end
