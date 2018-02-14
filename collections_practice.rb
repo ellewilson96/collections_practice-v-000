8f8c66e2a0fbd549bd6d353fce0f53e8e855ff1b
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-14 20:34:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,15 @@ def sort_array_asc(array)
     end
 end
 end
+
+def sort_array_asc(array)
+  array.sort do |b, a|
+    if a == b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+end
+end
