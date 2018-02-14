45b59b1c697f04d0fd3517b318f4dad345235d1c
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-14 20:44:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,7 +26,7 @@ def sort_array_char_count(array)
   array.sort_by {|x| x.length}
 end
 
-def swap_elements_from_to(array, x, y)
+def swap_elements(array, x, y)
   array[x], array[y] = array[y], array[x]
 array
 end
